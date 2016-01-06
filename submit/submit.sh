fbbb19efe88cd0b10a42f7b45cc614a23b3dfcc2
Erika Pauwels <erika.pauwels@tenforce.com>
2016-01-06 15:09:09 +0100

    Export SPARK_MASTER_URL as environment variable

--- submit/submit.sh
+++ submit/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
+export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
 
 echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
 echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
