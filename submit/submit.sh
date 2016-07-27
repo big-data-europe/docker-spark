09e361f26abecba31675cd55b7b9f6e7819f21f0
Casper Van Gheluwe <casper.vangheluwe@ugent.be>
2016-07-27 12:39:01 +0200

    Spark 1.6 requires SPARK_HOME to be set to /spark for correct execution

--- submit/submit.sh
+++ submit/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
+export SPARK_HOME=/spark
 
 /wait-for-step.sh
 
--- worker/worker.sh
+++ worker/worker.sh
@@ -6,6 +6,8 @@
 
 mkdir -p $SPARK_WORKER_LOG
 
+export SPARK_HOME=/spark
+
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
     --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
 
