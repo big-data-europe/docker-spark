32d04d1ddd0fa35fd014a86279597b59f4404577
Ivan Ermilov <earthquakesan@gmail.com>
2017-12-26 10:33:04 +0100

    added wait-for-it

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+current_branch := $(shell git rev-parse --abbrev-ref HEAD)
+build:
+	docker build -t bde2020/spark-base:$(current_branch) ./base
+	docker build -t bde2020/spark-master:$(current_branch) ./master
+	docker build -t bde2020/spark-worker:$(current_branch) ./worker
+	docker build -t bde2020/spark-submit:$(current_branch) ./submit
+
 network:
 	docker network create spark-net
 
--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -29,4 +29,40 @@ function configure() {
 }
 
 configure /opt/hive/conf/hive-site.xml hive HIVE_SITE_CONF
+
+function wait_for_it()
+{
+    local serviceport=$1
+    local service=${serviceport%%:*}
+    local port=${serviceport#*:}
+    local retry_seconds=5
+    local max_try=100
+    let i=1
+
+    nc -z $service $port
+    result=$?
+
+    until [ $result -eq 0 ]; do
+      echo "[$i/$max_try] check for ${service}:${port}..."
+      echo "[$i/$max_try] ${service}:${port} is not available yet"
+      if (( $i == $max_try )); then
+        echo "[$i/$max_try] ${service}:${port} is still not available; giving up after ${max_try} tries. :/"
+        exit 1
+      fi
+      
+      echo "[$i/$max_try] try in ${retry_seconds}s once again ..."
+      let "i++"
+      sleep $retry_seconds
+
+      nc -z $service $port
+      result=$?
+    done
+    echo "[$i/$max_try] $service:${port} is available."
+}
+
+for i in "${SERVICE_PRECONDITION[@]}"
+do
+    wait_for_it ${i}
+done
+
 exec $@
