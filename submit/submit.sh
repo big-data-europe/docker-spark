f8b0318082309d02e0c619b84df2ad4c26ee613d
Ivan Ermilov <earthquakesan@gmail.com>
2018-01-07 20:06:18 +0100

    add jvm profiler

--- submit/submit.sh
+++ submit/submit.sh
@@ -6,6 +6,7 @@ echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPA
 echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
 
 /spark/bin/spark-submit \
+    ${SPARK_ADDITIONAL_CONF} \
     --class ${SPARK_APPLICATION_MAIN_CLASS} \
     --master ${SPARK_MASTER_URL} \
     ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
