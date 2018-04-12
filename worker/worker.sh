9eeeb10202749fa0375f2a9de96a27b6b4efa93b
Tony Nyurkin <anyurkin@kaaiot.io>
2018-04-12 13:39:25 +0300

    Redirect spark-worker & spark-master logs to stdout

--- worker/worker.sh
+++ worker/worker.sh
@@ -8,6 +8,7 @@ mkdir -p $SPARK_WORKER_LOG
 
 export SPARK_HOME=/spark
 
+ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
     --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
-
