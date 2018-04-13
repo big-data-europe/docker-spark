3af9d16202d3e910354025cd7ea6f16b192c7edd
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-04-14 00:47:15 +0200

    Write container logs to stdout, as suggested by #26

--- master/master.sh
+++ master/master.sh
@@ -10,5 +10,7 @@ mkdir -p $SPARK_MASTER_LOG
 
 export SPARK_HOME=/spark
 
+ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
+
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
