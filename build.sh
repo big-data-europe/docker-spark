2715d1947e8c7b4721b741b1844df4b948d99bc6
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-04-14 00:26:01 +0200

    Add build.sh -- help for local build and docker-compose.yml

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+TAG=2.3.0-hadoop2.7
+
+build() {
+    NAME=$1
+    IMAGE=bde2020/spark-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    docker build -t $IMAGE .
+    cd -
+}
+
+build base
+build master
+build worker
+build submit
+build java-template template/java
+build python-template template/python
