08e125e7e9dc139e99f5c726ea2f0b342aeeadbe
Ivan Ermilov <earthquakesan@gmail.com>
2018-10-05 16:53:42 +0200

    spark 2.3.2 with hadoop 3.1.1

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -28,7 +28,31 @@ function configure() {
     done
 }
 
+function configure_hadoop() {
+    local path=$1
+    local module=$2
+    local envPrefix=$3
+
+    local var
+    local value
+
+    echo "Configuring $module"
+    for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
+        name=`echo ${c} | perl -pe 's/___/-/g; s/__/@/g; s/_/./g; s/@/_/g;'`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty /etc/hadoop/$module-site.xml $name "$value"
+    done
+}
+
 configure /opt/hive/conf/hive-site.xml hive HIVE_SITE_CONF
+configure_hadoop /etc/hadoop/core-site.xml core CORE_CONF
+configure_hadoop /etc/hadoop/hdfs-site.xml hdfs HDFS_CONF
+configure_hadoop /etc/hadoop/yarn-site.xml yarn YARN_CONF
+configure_hadoop /etc/hadoop/httpfs-site.xml httpfs HTTPFS_CONF
+configure_hadoop /etc/hadoop/kms-site.xml kms KMS_CONF
+configure_hadoop /etc/hadoop/mapred-site.xml mapred MAPRED_CONF
 
 function wait_for_it()
 {
