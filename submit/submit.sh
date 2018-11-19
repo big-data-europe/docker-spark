ebc3e204dfc5452d011833918610708a080f9358
alex <alex@armis.com>
2018-11-19 09:18:36 +0200

    added support for submitting application jar by url

--- submit/submit.sh
+++ submit/submit.sh
@@ -3,11 +3,22 @@
 export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
 export SPARK_HOME=/spark
 
+function is_uri() {
+    regex='(https?|hdfs|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+    if [[ $1 =~ $regex ]]
+    then 
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+
 /wait-for-step.sh
 
 
 /execute-step.sh
-if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
+if [[ `is_uri $SPARK_APPLICATION_JAR_LOCATION`=="true" || -f "${SPARK_APPLICATION_JAR_LOCATION}" ]]; then
     echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
     echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
     /spark/bin/spark-submit \
@@ -16,7 +27,7 @@ if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
         ${SPARK_SUBMIT_ARGS} \
         ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
 else
-    if [ -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
+    if [[ `is_uri $SPARK_APPLICATION_PYTHON_LOCATION`=="true" || -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]]; then
         echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URL}"
         echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
         PYSPARK_PYTHON=python3 /spark/bin/spark-submit \
