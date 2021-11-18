724099ba4c840a2a64f6e390713578a31c553711
Gezim Sejdiu <g.sejdiu@gmail.com>
2021-11-18 22:29:30 +0100

    Revert support_application_url #51, closes #139

--- submit/submit.sh
+++ submit/submit.sh
@@ -3,22 +3,10 @@
 export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
 export SPARK_HOME=/spark
 
-function is_uri() {
-    regex='(https?|hdfs|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
-    if [[ $1 =~ $regex ]]
-    then 
-        echo "true"
-    else
-        echo "false"
-    fi
-}
-
-
 /wait-for-step.sh
-
-
 /execute-step.sh
-if [[ `is_uri $SPARK_APPLICATION_JAR_LOCATION`=="true" || -f "${SPARK_APPLICATION_JAR_LOCATION}" ]]; then
+
+if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
     echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
     echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
     # Uses eval so that parameters in quotes could be passed, relevant for --conf parameters
@@ -28,7 +16,7 @@ if [[ `is_uri $SPARK_APPLICATION_JAR_LOCATION`=="true" || -f "${SPARK_APPLICATIO
         ${SPARK_SUBMIT_ARGS} \
         ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS})
 else
-    if [[ `is_uri $SPARK_APPLICATION_PYTHON_LOCATION`=="true" || -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]]; then
+    if [ -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
         echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URL}"
         echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
         PYSPARK_PYTHON=python3  /spark/bin/spark-submit \
@@ -39,4 +27,5 @@ else
         echo "Not recognized application."
     fi
 fi
+
 /finish-step.sh
